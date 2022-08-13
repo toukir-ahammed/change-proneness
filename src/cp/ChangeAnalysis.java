abbae0f481a0adf88ffc0c58c72d3c04d7356796
Toukir Ahammed <bsse0806@iit.du.ac.bd>
2022-08-14 00:37:38 +0600

    output format updated

--- src/cp/ChangeAnalysis.java
+++ src/cp/ChangeAnalysis.java
@@ -3,6 +3,8 @@ package cp;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import utils.GitUtil;
@@ -11,10 +13,12 @@ import utils.MyFileUtils;
 public class ChangeAnalysis {
 	private String cwd;
 	private String repo;
+	private HashMap<String, HashMap<String, Integer>> df;
 
 	public ChangeAnalysis(String repo, String cwd) {
 		this.repo = repo;
 		this.cwd = cwd;
+		this.df = new HashMap<>();
 	}
 
 	public void analyse(String base, String head, String project) throws IOException {
@@ -23,13 +27,28 @@ public class ChangeAnalysis {
 		System.out.println("Commits: " + commits.length);
 
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("Project\tRevision(prev)\tRevision(cur)\tAdded\tDeleted\tFile\n");
+		stringBuilder.append("Project,Revision(prev),Revision(cur),Added,Deleted,File, Total\n");
 
 		String[] changes = GitUtil.getChanges(base, head, repo);
 		System.out.println("Number of changed files: "+ changes.length);
 		for (String change : changes) {
-			stringBuilder.append(project + "\t" + base + "\t" + head + "\t" + change);
+			String added = change.split("\t")[0];
+			String deleted = change.split("\t")[1];
+			String file = change.split("\t")[2];
+			int total = Integer.parseInt(added) + Integer.parseInt(deleted);
+			stringBuilder.append(project + "," + base + "," + head + "," + change.replace('\t', ',') + "," + total);
 			stringBuilder.append(System.lineSeparator());
+			
+			if (df.containsKey(file)) {
+				HashMap<String, Integer> tempMap = df.get(file);
+				tempMap.put(head, total);
+				df.put(file, tempMap);
+			} else {
+				HashMap<String, Integer> tempMap = new HashMap<>();
+				tempMap.put(head, total);
+				df.put(file, tempMap);
+			}
+			
 		}
 
 		MyFileUtils.writeToFile(new File(cwd + File.separator + "changes-" + base  + "--" + head  + ".csv"), stringBuilder.toString());
@@ -47,5 +66,41 @@ public class ChangeAnalysis {
 			System.out.println("Analysing " + base + ".." + head);
 			analyse(base, head, project);
 		}
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Class");
+		
+		for (int i = 1; i < revisions.size(); i++) {
+			stringBuilder.append(",");
+			stringBuilder.append(revisions.get(i));
+		}
+		
+		stringBuilder.append(System.lineSeparator());
+		
+		for (Map.Entry<String, HashMap<String, Integer>> mapElement : df.entrySet()) {
+			
+			String file = mapElement.getKey();
+			
+			stringBuilder.append(file);
+			
+			HashMap<String, Integer> tempMap = mapElement.getValue();
+			
+			for (String string : revisions) {
+				if (tempMap.containsKey(string)) {
+					stringBuilder.append(",");
+					stringBuilder.append(tempMap.get(string));
+					
+				} else {
+					stringBuilder.append(",");
+					stringBuilder.append(0);
+				}
+			}
+			
+			stringBuilder.append(System.lineSeparator());
+			
+		}
+		
+		MyFileUtils.writeToFile(new File(cwd + File.separator + project + "-cp" + ".csv"), stringBuilder.toString());
+
 	}
 }
